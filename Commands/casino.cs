9e0540631665629924188b1695f1a6ade61c98dc
Vincent Nguyen <43223281+NguyenVincentxlll3@users.noreply.github.com>
2022-11-10 17:34:44 -0800

    Add files via upload
    Added casino, money, roles, reminders, stonks

---
+++ Commands/casino.cs
@@ -0,0 +1,134 @@
+﻿using _254DiscordBot;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWaggles.Commands
+{
+    public class casino : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("bet")]
+        public async Task BetAsync(long amount)
+        {
+            string balanceString = DBCommands.getMoneyBalance(Context.User.Id, Context.Guild.Id);
+            if (balanceString == "NONE" || balanceString == "0")
+            {
+                await ReplyAsync("You dont have any money to bet!");
+                return;
+            }
+            if (amount < 2)
+            {
+                await ReplyAsync("You have to bet more than that! Cheapskate.");
+                return;
+            }
+            
+            long senderBal = long.Parse(balanceString);
+            if (amount > senderBal || amount <= 0)
+            {
+                await ReplyAsync("Sorry, your balance of " + senderBal + " Bits is too low!");
+                return;
+            }
+            Random rand = new Random();
+            int chosenNum = rand.Next(0, 100);
+            int rowsAffected = 0;
+            //give it a 40% chance to succeed!
+            if (chosenNum < 41)
+            {
+                rowsAffected = DBCommands.payMoney(Context.User.Id, amount, Context.Guild.Id);
+                if (rowsAffected == 1)
+                {
+                    await ReplyAsync("Congrats! You won " + amount + " Bits, bringing your balance to " + (long.Parse(balanceString) + amount) + "Bits!");
+                }
+                else
+                {
+                    await ReplyAsync("An error occurred! Contact Hoovier!");
+                }
+            }
+            else
+            {
+                rowsAffected = DBCommands.payMoney(Context.User.Id, -amount, Context.Guild.Id);
+                if (rowsAffected == 1)
+                {
+                    await ReplyAsync("Oof. You lost " + amount + " Bits, bringing your balance to " + (long.Parse(balanceString) - amount) + "Bits!");
+                }
+                else
+                {
+                    await ReplyAsync("An error occurred! Contact Hoovier!");
+                }
+            }
+        }
+
+        [Command("slots")]
+        public async Task playSlots(long amount)
+        {
+            //check if user has the bits
+            string balanceString = DBCommands.getMoneyBalance(Context.User.Id, Context.Guild.Id);
+            if (balanceString == "NONE" || balanceString == "0")
+            {
+                await ReplyAsync("You dont have any money to bet!");
+                return;
+            }
+            if (amount < 2)
+            {
+                await ReplyAsync("You have to bet more than that! Cheapskate.");
+                return;
+            }
+            long senderBal = long.Parse(balanceString);
+            if (amount > senderBal || amount <= 0)
+            {
+                await ReplyAsync("Sorry, your balance of " + senderBal + " Bits is too low!");
+                return;
+            }
+            //hard codes the emojis that will correspond with every random number
+            Dictionary<int, string> emojiDic = new Dictionary<int, string>
+            {
+                {0, ":lemon:" },
+                {1, ":cherries:" },
+                {2, ":seven:" },
+                {3, ":star:" },
+                {4, ":bell:" }
+            };
+            int rowsAffected;
+            List<int> chosenNumbers = new List<int>();
+            Random rand = new Random();
+            string response = "";
+            for (int i = 0; i < 3; i++)
+            {
+                //gets a random number and stores it in the list
+                chosenNumbers.Add(rand.Next(5));
+                //gets random number generated from list.
+                response += emojiDic[chosenNumbers[i]] + " ";
+            }
+            //all match
+            if (chosenNumbers[0] == chosenNumbers[1] && chosenNumbers[2] == chosenNumbers[1])
+            {
+                await ReplyAsync(response);
+                rowsAffected = DBCommands.payMoney(Context.User.Id, amount * 3, Context.Guild.Id);
+                await ReplyAsync("3 matches! You win " + (amount * 3) + " Bits, bringing your balance to " + (long.Parse(balanceString) + (amount * 3)) + "Bits!");
+                return;
+            }
+            // if 0 matches 1, if 1 matches 2, or 0 matches 2, but not all three
+            else if (chosenNumbers[0] == chosenNumbers[1] || chosenNumbers[2] == chosenNumbers[1] || chosenNumbers[0] == chosenNumbers[2])
+            {
+                await ReplyAsync(response);
+                rowsAffected = DBCommands.payMoney(Context.User.Id, amount, Context.Guild.Id);
+                await ReplyAsync("2 matches! You win " + amount + " Bits, bringing your balance to " + (long.Parse(balanceString) + amount) + "Bits!");
+                return;
+            }
+            else
+            {
+                await ReplyAsync(response);
+                rowsAffected = DBCommands.payMoney(Context.User.Id, -amount, Context.Guild.Id);
+                await ReplyAsync("Whoops, you lost " + amount + "Bits, bringing your balance to " + (long.Parse(balanceString) - amount) + "Bits!");
+            }
+        }
+
+       
+
+        }
+}
---
+++ Commands/reactionRoles.cs
@@ -0,0 +1,74 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _254DiscordBot.Commands
+{
+    public class reactionRoles : ModuleBase<SocketCommandContext>
+    {
+        [Command("addrole")]
+        [RequireUserPermission(ChannelPermission.ManageRoles)]
+        public async Task addRole(Discord.IRole role, string emoji, ulong messageID)
+        {
+            //try to give user role in order to test if perms are present
+            IGuildUser user = (IGuildUser)Context.Message.Author;
+            try
+            {
+                //if user has it, remove then add back, this prevents removal of roles from people who already had a role
+                if (user.RoleIds.Contains(role.Id))
+                {
+                    await user.RemoveRoleAsync(role);
+                    await user.AddRoleAsync(role);
+                }
+                //if they dont, add it and then remove again
+                else
+                {
+                    await user.AddRoleAsync(role);
+                    await user.RemoveRoleAsync(role);
+                }
+                Console.WriteLine("Successful Role addition test. Permissions ok.");
+                //add a task to list
+                DBCommands.setReactionRole(role.Id, Context.Guild.Id, emoji, messageID);
+                await ReplyAsync("Success! Reacting with ``" + emoji + "`` will give the user the ``" + role.Name + "`` role!");
+            }
+            catch
+            {
+                await ReplyAsync("Sorry! Something went wrong! Make sure that I have permission to modify roles and that my role is higher than the selected one!");
+            }
+
+        }
+
+        [Command("removerole")]
+        [RequireUserPermission(ChannelPermission.ManageRoles)]
+        public async Task removeRole(Discord.IRole role)
+        {
+            //remove a task from the database!
+            string result = DBCommands.removeRole(role.Id);
+            await ReplyAsync(result);
+        }
+
+        [Command("listrole")]
+        [Alias("listroles", "lr")]
+
+        public async Task listRoles()
+        {
+            string response = "**__Role Reactions for this server:__** \n";
+            Dictionary<ulong, string> result = DBCommands.listRoles(Context.Guild.Id);
+            if (result.ContainsKey(0))
+            {
+                await ReplyAsync(response + result[0]);
+                return;
+            }
+            foreach (KeyValuePair<ulong, string> pair in result)
+            {
+                //get role name!
+                response = response + Context.Guild.GetRole(pair.Key).Name + " " + pair.Value + "\n";
+            }
+            await ReplyAsync(response);
+        }
+    }
+}
